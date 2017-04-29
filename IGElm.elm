1f4825413a843e6b02afb65365e7fbc5632a49fb
CorvusDiabolos <corvusdemuerte@comcast.net>
2017-04-28 23:28:44 -0600

    release 2 changes

--- IGElm.elm
+++ IGElm.elm
@@ -1,128 +1,209 @@
-import Keyboard exposing (..)
-import AnimationFrame
 import Html exposing (Html, button, div, input, program)
-import Html as App
+import String
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Time exposing (Time, second, millisecond)
+import Html.Attributes exposing (style)
+{-score in the game is based on the passage of time; trying to prepare for keeping score and possibly using time as my means of generating the objects in the level, but still not working quite right, hoping for the next release because I apparently don't fully understand how to utilize time-}
+import Time exposing (Time, second, millisecond) 
 import Keyboard
 
+{-halfSecond : Time
+halfSecond = millisecond * 500-}
+screenY : Float --for the gameplay area size; helpful in positioning other elements
+screenY = 400
+screenX : Float
+screenX = 700
 
-main = program
- { init = (initialModel, Cmd.none),
-   view = view,
-   subscriptions = subscriptions,
-   update = updateWithCommand }
+cubeX : Float --the object players use
+cubeX = 200
+cubeSize : Float
+cubeSize = 40
 
+{-platforms being weird-}
+platformSize : Float
+platformSize = cubeSize
 
-type Msg = KeyMsg Key | Tick Time
-type Key = KeyUp Int | KeyDown Int
-type alias Player =
-    { playerHeight : Float, jumping : Bool }
+cubeAcceleration : Float --the gravity and velocity shiz
+cubeAcceleration = 10
+triangleSpeed : Float
+triangleSpeed = 11
+maxJump : Float
+maxJump = 270
+
+floor : Float
+floor = screenY - cubeSize
+
+main = program 
+    { init = init,
+    view = view, 
+    update = update,
+    subscriptions = subscriptions }
+
 type alias Position =
     { x: Float, y: Float }
 
+type alias Platform = 
+    {platformHeight : Float}
 
-initialModel = { x = 0, vx = 0,
-                 y = 0, vy = 0,
-                 groundAcceleration = 0 }
+type alias Cube =
+    { cubeHeight : Float, jumping : Bool }
+
+type alias Triangle =
+    { position : Position }
+
+type alias Score = --score not quite working with Time not being my friend
+    Int
+
+type alias Delay = --more time stuff; may be a tool with generating the obstacles
+    Int
+
+type Model =
+    InsertCoin Score | Started Cube (List Triangle) Score
 
 
+init : (Model, Cmd Msg)
+init = (InsertCoin 0, Cmd.none)
 
-subscriptions model =
-    Sub.batch
-    [ Keyboard.downs (KeyDown >> KeyMsg)
-    , Keyboard.ups (KeyUp >> KeyMsg)
-    --https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame.
-    , AnimationFrame.diffs Tick]
 
-updateWithCommand msg model =
-    (update msg model, Cmd.none)
+type Msg = Tick Time | SpaceBar Keyboard.KeyCode | AddTriangle
 
+checkCollision : Cube -> Triangle -> Bool --Very, very rough, but mostly ok
+checkCollision cube triangle =
+    let
+        collisionX = (cubeX > triangle.position.x) && (cubeX < triangle.position.x + cubeSize)
+        collisionY = (cube.cubeHeight + cubeSize > triangle.position.y) && (cube.cubeHeight < triangle.position.y + cubeSize)
+    in
+        collisionX && collisionY
+
+addTriangle : List Triangle -> List Triangle --recursive list to keep the game going until the player dies
+addTriangle triangles1 =
+    let
+        triangles = List.map (\triangle -> { position={ x=triangle.position.x-triangleSpeed, y=triangle.position.y } }) triangles1
+        previousTriangle = case List.head triangles of
+            Nothing ->
+                { position={ x=0, y=0 } }
+            Just val ->
+                val
+        previousTriangleX = previousTriangle.position.x
+    in
+        if previousTriangleX < screenX-cubeSize-400 
+        {-to give some space between triangles otherwise you get a long train of them and instantly die-} 
+            then List.append [{ position={ x=screenX, y=floor }}] triangles
+        else
+       triangles
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Tick time -> tick model
-        KeyMsg k -> key k model
+    case model of
+        InsertCoin score ->
+            case msg of
+                SpaceBar key -> --32 is Spacebar
+                    if key == 32 then ( Started (Cube floor False) [] 0, Cmd.none )
+                    else ( model, Cmd.none )
+                addTriangle ->
+                    ( model, Cmd.none )
+        Started cube triangles score ->
+            let
+                checkCubeCollision = checkCollision cube
+                collisions = List.map checkCubeCollision triangles
+                isGameOver = List.member True collisions
 
---horizontalVelocity = 3
-gravityAcceleration = 0.3
-jumpVelocity = 6
+                cubeHeight = cube.cubeHeight
+                jumping = (
+                    if not cube.jumping || cube.cubeHeight <= maxJump then
+                        False
+                    else True
+                )
+                newHeight = (
+                    if jumping then cubeHeight - cubeAcceleration
+                    else if cubeHeight < floor then cubeHeight + cubeAcceleration
+                    else cubeHeight
+                )
+            in
+                case msg of
+                    Tick _ ->
+                        if isGameOver then
+                            ( InsertCoin score, Cmd.none )
+                        else
+                        (
+                            Started { cube | cubeHeight = newHeight, jumping = jumping }
+                            (addTriangle triangles) (score + 1),
+                            Cmd.none
+                        )
+                    SpaceBar key ->
+                        if key == 32 && cubeHeight == floor then
+                            ( Started { cube | jumping = True } triangles score, Cmd.none )
+                        else (Started cube triangles score, Cmd.none)
+                    addTriangle ->
+                        ( model, Cmd.none )
 
 
-key msg model =
-    model
-        |> case msg of
-            -- Spacebar
-            KeyDown 32 -> jump
-            
-            --left these options just in case I change my mind about what game to develop
-     {-       -- Left arrow 
-            KeyDown 37 -> accelerateTo -horizontalVelocity
-            KeyUp   37 -> stop (<)
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch([
+        Keyboard.presses SpaceBar,
+        Time.every (millisecond*20) Tick
+    ])
 
-            -- Right arrow
-            KeyDown 39 -> accelerateTo horizontalVelocity
-            KeyUp   39 -> stop (>)-}
 
-            _ -> identity
+containerStyle : List (String, String)
+containerStyle =
+    [
+        ("display", "flex"),
+        ("align-items", "center"),
+        ("justify-content", "center"),
+        ("height", "100%")
+    ]
 
-accelerateTo velocity model =
-    {model | groundAcceleration = velocity}
+triangleTriangle : Triangle -> Html a
+triangleTriangle triangle =
+    let
+        leftPoint = "0," ++ toString cubeSize
+        rightPoint = toString cubeSize ++ "," ++ toString cubeSize
+        topPoint = toString (cubeSize / 2) ++ ",0"
+        pointList = [leftPoint, topPoint, rightPoint]
+    in
+        polygon [width (toString cubeSize), height (toString cubeSize), fill "black",
+                 transform ("translate(" ++ toString(triangle.position.x) ++ "," ++ toString(triangle.position.y) ++ ")"),
+                 points (String.join " " pointList)] []
 
-jump model =
-    if model.vy == 0 then
-        {model | vy = jumpVelocity}
-    else
-        model
+cubeRect : Cube -> Html a --need to figure out the jump/twist animation; lord help me
+cubeRect cube =
 
-stop compare model =
-    if compare model.groundAcceleration 0 then
-       {model | groundAcceleration = 0}
-    else
-        model
+        rect [width (toString cubeSize), height (toString cubeSize), fill "orange",
+              y (toString cube.cubeHeight), x (toString cubeX)] []
 
-tick model =
-    model
-        |> traction
-        |> gravity
-        |> motion
-        |> floors
+gameContainer : List (Html msg) -> Html msg
+gameContainer children =
+    div [ Html.Attributes.style containerStyle ]
+        [
+            svg [
+                width (toString screenX),
+                height (toString screenY)
+            ]
+            (List.concat [
+                [rect [width "100%", height "100%", fill "teal"] []],
+                children
+            ])
+        ]
 
-traction model =
-    if model.y == 0 then
-       {model | vx = model.groundAcceleration}
-    else
-       model
-
-gravity model =
-    {model | vy = model.vy - gravityAcceleration}
-
-motion model =
-    {model | x = model.x + model.vx,
-             y = model.y + model.vy}
-
-floors model =
-    if model.y < 0 then
-       {model | y = 0, vy = 0}
-    else
-       model
-
+view : Model -> Html Msg
 view model =
-    svg
-      -- List of attributes of SVG node
-      [ width "100%", height "100%" -- Scale up to take the full page
-      , viewBox "0 0 500 100" -- Numbers used in the drawing are relative to these viewBox dimensions. 0 in the y dimension is the top of the drawing.
-      ]
-      -- List of children
-      [ rect [ 
-          x (toString (model.x + 100))
-          , y (toString (model.y + 69.5))
-          , width "22"
-          , height "22"
-          , stroke "orange"
-          , fill "orange"
-      ] []
-      , line [ x1 "0", y1 "92"
-             , x2 "500", y2 "95"
-             , strokeWidth "2"
-             , stroke "gray" ] [] ]
+    case model of
+        InsertCoin score ->
+            gameContainer [
+                text_ [x (toString (screenX/2)),
+                       y (toString (screenY/2)),
+                       fill "white",
+                       textAnchor "middle"
+                      ] [ text "Press Spacebar to insert coin" ]
+            ]
+        Started cube triangles score ->
+                gameContainer
+                    (List.concat [
+                        [
+                            cubeRect cube
+                        ],
+                        List.map triangleTriangle triangles
+                    ])
